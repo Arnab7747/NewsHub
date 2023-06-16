d59c25bc51ea272a4e01d69d6c3f4586d39d8b76
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2023-06-16 23:12:24 +0530

    state in class

--- src/components/News.js
+++ src/components/News.js
@@ -1,21 +1,32 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import "./News.css";
 
 export class News extends Component {
+  constructor(){
+    super();
+    console.log("hello")
+    this.state={
+      
+    }
+  }
   render() {
     return (
       <div className="container my-3">
         <h2>News Hub - Top Head Lines</h2>
-        <div className="col-md-3">
-          <NewsItem title="mytitle" description="my desc" />
-        </div>
-        <div className="col-md-3">
-          <NewsItem title="mytitle" description="my desc" />
-        </div>
-        <div className="col-md-3">
-          <NewsItem title="mytitle" description="my desc" />
-        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="my desc"  imageUrl=""/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="my desc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="my desc" />
+          </div>
         
+          
+        </div>
       </div>
     );
   }
