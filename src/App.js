6a4bcfffcec20a70e2013242aa77ded9723fd82b
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2024-03-31 00:36:00 +0530

    homepage in jason

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
 
 export default class App extends Component {
   render() {
